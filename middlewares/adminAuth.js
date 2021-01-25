5781e94d89a700c56588c7968deba6758d51d86f
FelipeSchulzLuz <schulz.neo@gmail.com>
2021-01-25 20:05:13 -0300

    corrigindo comportamento do css

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -1,12 +1,12 @@
 const User = require("../user/User");
 
-function adminAuth(req,res,next){
-   if(req.session.user != undefined) {
-       next()
-   }else {
-       console.log("falha ao autenticar");
-       res.redirect("/")
-   }
+function adminAuth(req, res, next) {
+    if (req.session.user != undefined) {
+        next()
+    } else {
+        console.log("Falha ao autenticar");
+        res.redirect("/login")
+    }
 }
 
 module.exports = adminAuth
